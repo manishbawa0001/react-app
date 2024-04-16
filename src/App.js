f334e4fceba8fb7c69e4bde11232a93793b33b5e
manishbawa0001 <manishbawa940@gmail.com>
2024-04-16 06:57:49 -0700

    changing content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p style={{fontWeight: 'bold', textAlign: 'center'}}>
-          Welcome, It's Manish here. Applied autoscaling this time.
+          Welcome, It's Manish here.
         </p>
       </header>
     </div>
