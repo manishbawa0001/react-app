b4060dd4f39bdacef66a9bf84e21efcfd5ff8dbb
manishbawa0001 <manishbawa940@gmail.com>
2024-04-16 05:13:04 -0700

    newcontext

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p style={{fontWeight: 'bold', textAlign: 'center'}}>
-          Welcome, It's Manish here. Applied autoscaling.
+          Welcome, It's Manish here. Applied autoscaling this time.
         </p>
       </header>
     </div>
