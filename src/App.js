5618451f3a4a2f71cb60cb88a898258f2d5fc341
manishbawa0001 <manishbawa940@gmail.com>
2024-04-16 04:47:33 -0700

    autoscaling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p style={{fontWeight: 'bold', textAlign: 'center'}}>
-          Welcome, It's Manish here
+          Welcome, It's Manish here. Applied autoscaling.
         </p>
       </header>
     </div>
